6ec96313f42dc5299eb55bc0236c6531d38f1169
josephboi123 <jevaldez101@student.hbuhsd.edu>
2025-10-27 18:21:21 +0000

    Control F

--- Main.java
+++ Main.java
@@ -5,16 +5,43 @@ public class Main
   public static void main(String[] args)
   {
     // Make sure to test your methods here
+
+    printNTimes("hello", 5);
+
+
+
+
+
+
+
   }
 
   public static void printNTimes(String word, int N)
   {
     // code solution here
+
+    for(int i = 0; i < word.length(); i++)
+    {
+      String current_letter = word.substring(i,i+1); 
+      for ( int j = 0; j < N; j++)
+{ 
+  System.out.println(current_letter);
+}
+
+    }
   }
+
+
 
   public static void printNums()
   {
     // code solution here
+    for( int i = 0; word.length(); i++) 
+    {
+
+
+
+
   }
 
   public static void uprightNumberTriangle(int N)
